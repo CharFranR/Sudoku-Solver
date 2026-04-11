2e606c3694b4f3dc215b5a67a0d9950f5168b0d3
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-04-10 22:02:05 -0600

    fix(test): fix test runner to properly load and run all plunit suites

--- src/run_tests.pl
+++ src/run_tests.pl
@@ -1,8 +1,8 @@
-:- module(run_tests, [run_all_tests/0]).
+:- use_module(library(plunit)).
+
+:- ['src/sudoku_validate.plt', 'src/sudoku_solver.plt'].
 
 %% run_all_tests is semidet.
 %  Loads all test suites and runs them. Fails if any test fails.
 run_all_tests :-
-    consult('src/sudoku_validate.plt'),
-    consult('src/sudoku_solver.plt'),
-    run_tests([]).
+    plunit:run_tests([sudoku_validate, sudoku_solver]).
