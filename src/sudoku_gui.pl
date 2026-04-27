f582edec7c6abfa31483342855a027250766cb1f
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-04-26 20:07:10 -0600

    fix(gui): open file dialog first, then focus text_item, then confirm

--- src/sudoku_gui.pl
+++ src/sudoku_gui.pl
@@ -337,6 +337,8 @@ ask_file_path(Dialog, Mode, PathString) :-
     send(D, append, button(cancelar, message(D, return, @nil))),
     send(D, default_button, ok),
     send(D, transient_for, Dialog),
+    send(D, open_centered),
+    send(D, focus, TI),
     get(D, confirm_centered, Result),
     send(D, destroy),
     Result \== @nil,
